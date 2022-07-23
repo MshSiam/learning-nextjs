08e73dd309f29118603ba4ec4a6b543f4336344c
MshSiam <siyamkenway@gmail.com>
2022-07-23 15:52:15 +0600

    styled jsx used

--- pages/web/about.js
+++ pages/web/about.js
@@ -5,6 +5,22 @@ export default function about() {
     <div>
       <Navbar></Navbar>
       <h1>this is about</h1>
+      <p>
+        In that page, i have used styled jsx. this was fun. this is as like
+        enternal css.
+      </p>
+
+      {/* using styled jsx */}
+      <style jsx>
+        {`
+          h1 {
+            color: blue;
+          }
+          p {
+            color: #9f2b68;
+          }
+        `}
+      </style>
     </div>
   )
 }
