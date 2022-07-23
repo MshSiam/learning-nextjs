b84629811d9809eb0d661df07b20dacf1a967b0c
MshSiam <siyamkenway@gmail.com>
2022-07-23 15:44:56 +0600

    inline css used

--- pages/web/home.js
+++ pages/web/home.js
@@ -5,6 +5,10 @@ export default function home() {
     <div>
       <Navbar></Navbar>
       <h1>this is home</h1>
+      <p style={{ color: "green" }}>
+        In this page , i've used inline css. This is nothing new . we have to
+        use such in a way we have used in react.
+      </p>
     </div>
   )
 }
